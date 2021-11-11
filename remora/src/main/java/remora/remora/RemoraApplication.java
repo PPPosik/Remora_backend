a01b2ceb86161121e1e70186bcf33841378c8cb6
NotableDeveloper <castlereal@naver.com>
2021-11-11 13:53:29 +0900

    init project

---
+++ remora/src/main/java/remora/remora/RemoraApplication.java
@@ -0,0 +1,13 @@
+package remora.remora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoraApplication.class, args);
+	}
+
+}
---
+++ remora/src/test/java/remora/remora/RemoraApplicationTests.java
@@ -0,0 +1,13 @@
+package remora.remora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
