6f37c069a909d10ca20901fe3ff8ba7d7f87647d
NotableDeveloper <castlereal@naver.com>
2021-11-22 10:32:34 +0900

    Health controller javadoc 작성

--- remora/src/main/java/remora/remora/Health/HealthController.java
+++ remora/src/main/java/remora/remora/Health/HealthController.java
@@ -5,8 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 서버가 정상적으로 동작하는 지를 확인하기 위한 Healthy Controller
+ */
 @RestController
 public class HealthController {
+    /**
+     * @Author Seong Jin
+     * @Param
+     * @return Healthy
+     * @throws Exception
+     */
     @GetMapping("/health")
     @ResponseStatus(HttpStatus.OK)
     public String healthCheck() throws Exception {
