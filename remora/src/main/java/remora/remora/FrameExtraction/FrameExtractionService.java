737954da42459f173384419eb87e43c524a9df03
PPPosik <eoanswkgksk@gmail.com>
2021-11-28 00:08:04 +0900

    Update frame extraction to multithread

--- remora/src/main/java/remora/remora/FrameExtraction/FrameExtractionService.java
+++ remora/src/main/java/remora/remora/FrameExtraction/FrameExtractionService.java
@@ -1,49 +1,42 @@
 package remora.remora.FrameExtraction;
 
-import java.io.File;
 import java.util.ArrayList;
 
-import java.awt.image.BufferedImage;
-
-import javax.imageio.ImageIO;
-
-import org.jcodec.api.FrameGrab;
-import org.jcodec.common.io.NIOUtils;
 import org.jcodec.common.model.Picture;
-import org.jcodec.scale.AWTUtil;
 import org.springframework.stereotype.Service;
 
 import remora.remora.FrameExtraction.dto.FrameExtractionRequestDto;
 import remora.remora.FrameExtraction.dto.FrameExtractionResponseDto;
+import remora.remora.FrameExtraction.thread.ExtractionThread;
 
 @Service
 public class FrameExtractionService {
     public FrameExtractionResponseDto frameExtract(FrameExtractionRequestDto request) {
         FrameExtractionResponseDto response = new FrameExtractionResponseDto();
+        int frameInterval = 10;
+        int threadSize = 8;
+        ExtractionThread[] extractionThread = new ExtractionThread[threadSize];
+        boolean isRunning = true;
+
         response.success = false;
         response.frameSet = new ArrayList<Picture>();
 
+        for (int i = 0; i < threadSize; i++) {
+            extractionThread[i] = new ExtractionThread(i, threadSize, frameInterval, response, request.originVideo);
+            extractionThread[i].start();
+        }
+
         try {
-            FrameGrab grab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(request.originVideo));
-            int totalFrames = grab.getVideoTrack().getMeta().getTotalFrames();
-            int frameInterval = 10;
-            Picture picture = null;
+            while (isRunning) {
+                Thread.sleep(500);
+                isRunning = false;
 
-            System.out.println("totalFrames : " + totalFrames);
-
-            for (int i = 0; i < totalFrames; i += frameInterval) {
-                if ((picture = grab.seekToFramePrecise(i).getNativeFrame()) != null) {
-                    response.frameSet.add(picture);
-                    System.out.println(i + " " + picture.getWidth() + " " + picture.getHeight());
-
-                    BufferedImage bufferedImage = AWTUtil
-                            .toBufferedImage(response.frameSet.get((response.frameSet.size() - 1)));
-                    ImageIO.write(bufferedImage, "png",
-                            new File("C:\\testFrame\\frame" + (response.frameSet.size() - 1) + ".png"));
+                for (int i = 0; i < threadSize; i++) {
+                    if (extractionThread[i].isAlive()) {
+                        isRunning = true;
+                    }
                 }
             }
-
-            response.success = true;
         } catch (Exception e) {
             response.success = false;
             response.frameSet = null;
