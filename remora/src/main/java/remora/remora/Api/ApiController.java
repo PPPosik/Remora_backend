5eb115de291d4075ef064fba8c6a25077d8a10e1
NotableDeveloper <castlereal@naver.com>
2021-12-20 13:30:40 +0900

    Review 반영, Wild card import 수정

--- remora/src/main/java/remora/remora/Api/ApiController.java
+++ remora/src/main/java/remora/remora/Api/ApiController.java
@@ -1,15 +1,12 @@
 package remora.remora.Api;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import remora.remora.Api.dto.DeleteRequestDto;
 import remora.remora.Api.dto.SimpleResponseDto;
 import remora.remora.Api.dto.UploadRequestDto;
 import remora.remora.Api.dto.UploadResponseDto;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Video 업로드 및 응답을 받기 위한 Controller
--- remora/src/main/java/remora/remora/MainControl/MainController.java
+++ remora/src/main/java/remora/remora/MainControl/MainController.java
@@ -5,7 +5,13 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 import org.springframework.web.multipart.MultipartFile;
 import remora.remora.Api.ApiController;
