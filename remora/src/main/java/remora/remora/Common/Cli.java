be406a8bffc0c54b656da17ba3878de37fa05b7b
PPPosik <eoanswkgksk@gmail.com>
2021-12-26 19:56:12 +0900

    Update cli to synchronous

--- remora/src/main/java/remora/remora/Common/Cli.java
+++ remora/src/main/java/remora/remora/Common/Cli.java
@@ -5,9 +5,13 @@ import java.io.IOException;
 public class Cli {
     public static Boolean exec(String command, String args) {
         try {
-            Runtime.getRuntime().exec(String.format(command, args));
+            Process process = Runtime.getRuntime().exec(String.format(command, args));
+            process.waitFor();
             return true;
         } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } catch (InterruptedException e) {
             e.printStackTrace();
             return false;
         }
