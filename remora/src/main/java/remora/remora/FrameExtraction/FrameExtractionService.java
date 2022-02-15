de2a29266408c62c4afa9d70dbf5a55a9bde5eb4
PPPosik <eoanswkgksk@gmail.com>
2022-02-15 17:49:57 +0900

    Refactoring thread

--- remora/src/main/java/remora/remora/FrameExtraction/FrameExtractionService.java
+++ remora/src/main/java/remora/remora/FrameExtraction/FrameExtractionService.java
@@ -9,27 +9,19 @@ import remora.remora.FrameExtraction.thread.ExtractionThread;
 
 @Service
 public class FrameExtractionService {
-    public ArrayList<PicturePair> frameExtract(String path) throws InterruptedException {
-        ArrayList<PicturePair> ret = new ArrayList<PicturePair>();
-        int frameInterval = 150;
+    public ArrayList<PicturePair> frameExtract(String path) throws Exception {
         int threadSize = 2;
+        int frameInterval = 150;
+        ArrayList<PicturePair> ret = new ArrayList<>();
         ExtractionThread[] extractionThread = new ExtractionThread[threadSize];
-        Boolean isRunning = true;
 
         for (int i = 0; i < threadSize; i++) {
             extractionThread[i] = new ExtractionThread(i, threadSize, frameInterval, ret, path);
             extractionThread[i].start();
         }
 
-        while (isRunning) {
-            Thread.sleep(500);
-            isRunning = false;
-
-            for (int i = 0; i < threadSize; i++) {
-                if (extractionThread[i].isAlive()) {
-                    isRunning = true;
-                }
-            }
+        for (ExtractionThread thread : extractionThread) {
+            thread.join();
         }
 
         return ret;
