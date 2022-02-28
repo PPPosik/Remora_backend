06aa94bd14ea45c6d4cdd4c47e1784f190d76bd7
PPPosik <eoanswkgksk@gmail.com>
2022-02-28 11:06:06 +0900

    Update translation dto

--- remora/src/main/java/remora/remora/Translation/dto/TranslationResponseDto.java
+++ remora/src/main/java/remora/remora/Translation/dto/TranslationResponseDto.java
@@ -1,7 +1,18 @@
 package remora.remora.Translation.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TranslationResponseDto {
-    public Boolean success;
-    public String originText;
-    public String translatedText;
+    public boolean success;
+    public String message;
+    public List<String> text;
+    public List<String> translatedText;
+
+    public TranslationResponseDto() {
+        this.success = false;
+        this.message = null;
+        this.text = new ArrayList<>();
+        this.translatedText = new ArrayList<>();
+    }
 }
