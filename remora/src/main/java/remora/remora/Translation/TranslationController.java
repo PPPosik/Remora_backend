ae07900a84e17371afe430220de1182706221704
PPPosik <eoanswkgksk@gmail.com>
2022-02-28 11:06:58 +0900

    Update translation controller

--- remora/src/main/java/remora/remora/Translation/TranslationController.java
+++ remora/src/main/java/remora/remora/Translation/TranslationController.java
@@ -1,20 +1,47 @@
 package remora.remora.Translation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import remora.remora.Translation.dto.TranslationRequestDto;
 import remora.remora.Translation.dto.TranslationResponseDto;
 
+import javax.validation.Valid;
+
 @RestController
 public class TranslationController {
-    TranslationService translationService = new TranslationService();
+    private final TranslationService translationService;
 
-    public TranslationResponseDto translate(TranslationRequestDto request) {
+    @Autowired
+    public TranslationController(TranslationService translationService) {
+        this.translationService = translationService;
+    }
+
+    @GetMapping("/text/translated")
+    @ResponseBody
+    public TranslationResponseDto translate(@RequestBody @Valid TranslationRequestDto request) {
+        TranslationResponseDto response = new TranslationResponseDto();
+
         try {
-            return translationService.translate(request);
+            for (int i = 0; i < request.text.size(); i++) {
+                response.translatedText.add(translationService.translate(request.text.get(i), request.needTranslation.get(i)));
+                response.text.add(request.text.get(i));
+            }
+
+            response.success = true;
+            response.message = "Success";
         } catch (Exception e) {
             e.printStackTrace();
-            return null;
+
+            response.success = false;
+            response.message = e.toString();
+            response.text = null;
+            response.translatedText = null;
         }
+
+        return response;
     }
 }
