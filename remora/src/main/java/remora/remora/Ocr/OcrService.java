3f1616edaa23e16f2e1d728ee0e791cb354953b8
NotableDeveloper <castlereal@naver.com>
2022-03-02 00:21:34 +0900

    Oce Service Logging

--- remora/src/main/java/remora/remora/Ocr/OcrService.java
+++ remora/src/main/java/remora/remora/Ocr/OcrService.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,8 @@ public class OcrService {
     @Value("${ocr-result-path}")
     String ocrResultPath;
 
+    private Logger log = LoggerFactory.getLogger(getClass());
+
     public String detection(ArrayList<Integer> frameSet, String videoCode) throws Exception {
         StringBuilder args = new StringBuilder();
         StringBuilder ret = new StringBuilder();
@@ -23,24 +27,25 @@ public class OcrService {
         for (Integer n : frameSet) {
             args.append(videoCode + "_" + n + ".png ");
         }
-        System.out.println("args = " + args);
+
+        log.info("args = {}", args);
 
         if (Cli.exec(ocrModulePath, args.toString())) {
             BufferedReader reader = new BufferedReader(new FileReader(ocrResultPath));
 
             String str;
             while ((str = reader.readLine()) != null) {
-                System.out.println(str);
+                log.info("{}", str);
                 ret.append(str + "\n");
             }
             reader.close();
         } else {
-            System.out.println("Run OCR module error");
+            log.debug("Run OCR module error");
             throw new Exception("Run OCR module error");
         }
 
         if (ret.length() == 0) {
-            System.out.println("There is no text in video");
+            log.debug("There is no text in video");
             throw new Exception("There is no text in video");
         }
 
