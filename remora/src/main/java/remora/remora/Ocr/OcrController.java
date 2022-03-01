8bc580a56e886eaf0e86159652a48fcdd572883d
NotableDeveloper <castlereal@naver.com>
2022-03-02 00:38:13 +0900

    Ocr Controller logging

--- remora/src/main/java/remora/remora/Ocr/OcrController.java
+++ remora/src/main/java/remora/remora/Ocr/OcrController.java
@@ -1,5 +1,7 @@
 package remora.remora.Ocr;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,7 @@ import javax.validation.Valid;
 @RestController
 public class OcrController {
     private final OcrService ocrService;
+    private Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     public OcrController(OcrService ocrService) {
@@ -28,18 +31,19 @@ public class OcrController {
 
         try {
             if (request.frameSet.size() != request.videoCode.size()) {
+                log.debug("Request data length different. frameSet length : {}, videoCode : {}", request.frameSet.size(), request.videoCode.size());
                 throw new RequestDataLengthDifferentException("frameSet length : " + request.frameSet.size() + ", videoCode length : " + request.videoCode.size());
             }
 
             for (int i = 0; i < request.videoCode.size(); i++) {
                 response.text.add(ocrService.detection(request.frameSet.get(i), request.videoCode.get(i)));
             }
-
+            log.info("Frame extraction is {}", "success");
             response.success = true;
             response.message = "success";
         } catch (Exception e) {
             e.printStackTrace();
-
+            log.debug("Frame extraction is {}, Exception : {}", "fail", e.getMessage());
             response.success = false;
             response.message = e.toString();
             response.text = null;
