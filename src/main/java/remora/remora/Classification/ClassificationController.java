bfa80ebacdc2301bb715bec2cc8e4febb7f31359
PPPosik <eoanswkgksk@gmail.com>
2022-03-28 09:04:08 +0900

    Delete unused import

--- src/main/java/remora/remora/Classification/ClassificationController.java
+++ src/main/java/remora/remora/Classification/ClassificationController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
